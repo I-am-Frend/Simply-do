9b40b10e992f02ebdae2c14b270e6478c6072e84
I-am-Frend <>
2023-06-23 11:03:28 +0000

    Added input for to-do list

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+# Simple To-do list
+# Made by Frend
+
+# Task Input
+def add_task(tasks):
+    task = input("Enter task description: ")
+    tasks.append({"description": task, "complete": False})
+    print("Task added successfully!")
