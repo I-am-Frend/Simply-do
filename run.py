fdfb88c83877300c3b03d51ec9dd7f89ae95690e
I-am-Frend <>
2023-06-23 11:15:58 +0000

    Added Task completion function

--- run.py
+++ run.py
@@ -6,3 +6,17 @@ def add_task(tasks):
     task = input("Enter task description: ")
     tasks.append({"description": task, "complete": False})
     print("Task added successfully!")
+
+
+# Mark the task as complete
+def mark_task_complete(tasks):
+    print("Tasks:")
+    view_tasks(tasks)
+    index = int(input("Enter the index of the task to mark as complete: "))
+    if 0 <= index < len(tasks):
+        tasks[index]["complete"] = True
+        print("Task marked as complete!")
+    else:
+        print("Invalid task index.")
+
+
